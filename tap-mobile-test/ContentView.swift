90317abded04d1fd04b67356e5f90bc2c2fa9ff5
changemin <chgemn@gmail.com>
2022-01-31 21:11:14 +0900

    Initial Commit

---
+++ tap-mobile-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tap-mobile-test
+//
+//  Created by 변경민 on 2022/01/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tap-mobile-test/tap_mobile_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  tap_mobile_testApp.swift
+//  tap-mobile-test
+//
+//  Created by 변경민 on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct tap_mobile_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
